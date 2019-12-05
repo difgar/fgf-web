fa65d56e9a0e6d477c7ce683714a8e1cd23b5970
Diego Felipe Garcia Cortes <difgar@gmail.com>
2019-12-05 11:44:46 -0500

    Funcionalidad del Periodo

--- src/components/Navegation.jsx
+++ src/components/Navegation.jsx
@@ -1,42 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../assets/styles/components/Navegation.scss';
 import expandArrow from '../assets/statics/expandArrow.png';
 
-const Navegation = () => (
-  <nav className='navegation'>
-    <div className='navegation__account'>
-      <pre><a href='/'>Home</a></pre>
-      <pre>
-        {' '}
-/
-        {' '}
-        <a href='/'>Activos</a>
-      </pre>
-      <pre>
-        {' '}
-/
-        {' '}
-        <a href='/'>Bancos</a>
-      </pre>
-      <pre>
-        {' '}
-/
-        {' '}
-        <a href='/'>Bancolombia</a>
-      </pre>
-    </div>
-    <div className='navegation__period'>
-      <div className='navegation__period__current'>
-        <p>Periodo: 2018</p>
-        <img className='navegation__period__img' src={expandArrow} alt='' />
+const Navegation = () => {
+
+  const [selectedYear, setSelectedYear] = useState(2019);
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  const years = [];
+  for (let year = currentYear; year >= 2010; year--) {
+    years.push(year);
+  }
+
+  return (
+    <nav className='navegation'>
+      <div className='navegation__account'>
+        <pre><a href='/'>Home</a></pre>
+        <pre>
+          {' '}
+  /
+          {' '}
+          <a href='/'>Activos</a>
+        </pre>
+        <pre>
+          {' '}
+  /
+          {' '}
+          <a href='/'>Bancos</a>
+        </pre>
+        <pre>
+          {' '}
+  /
+          {' '}
+          <a href='/'>Bancolombia</a>
+        </pre>
       </div>
-      <div className='navegation__period__list'>
-        <p className='navegation__period__list__item'>2019</p>
-        <p className='navegation__period__list__item'>2018</p>
-        <p className='navegation__period__list__item'>2017</p>
+      <div className='navegation__period'>
+        <div
+          className='navegation__period__current'
+          onClick={() => { document.getElementsByClassName('navegation__period')[0].classList.add('active') ; }}
+          role='button'
+          tabIndex='0'
+        >
+          <p>{`Periodo: ${selectedYear}`}</p>
+          <img className='navegation__period__img' src={expandArrow} alt='' />
+        </div>
+        <div className='navegation__period__list'>
+          {years.map((year) => (
+            <p
+              className='navegation__period__list__item'
+              key={year}
+              onClick={() => {
+                setSelectedYear(year);
+                document.getElementsByClassName('navegation__period')[0].classList.remove('active');
+                console.log(document.getElementsByClassName('navegation__period')[0].classList);
+              }}
+            >
+              {year}
+            </p>
+          ))}
+        </div>
       </div>
-    </div>
-  </nav>
-);
+    </nav>
+  );
+};
 
 export default Navegation;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
+/* eslint-disable no-return-assign */
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducer from './reducers';
 import App from './routers/App';
+
+/*const API_SOCIOS = 'http://localhost:3000/socios';
+const API_SOCIEDADES = 'http://localhost:3000/sociedades';
+const API_TERCEROS = 'http://localhost:3000/terceros';
+const API_CUENTAS_VIGENTES = 'http://localhost:3000/cuentasVigentes';
+const API_CUENTAS_CANCELADAS = 'http://localhost:3000/cuentasCanceladas';
+
+let initialState = {};
+
+async function callAPI(API) {
+  const response = await fetch(API);
+  const data = await response.json();
+  return data;
+};
+
+callAPI(API_SOCIOS).then((socios) => initialState = { ...initialState, socios });
+callAPI(API_SOCIEDADES).then((sociedades) => initialState = { ...initialState, sociedades });
+callAPI(API_TERCEROS).then((terceros) => initialState = { ...initialState, terceros });
+callAPI(API_CUENTAS_VIGENTES).then((cuentasVigentes) => initialState = { ...initialState, cuentasVigentes });
+callAPI(API_CUENTAS_CANCELADAS).then((cuentasCanceladas) => initialState = { ...initialState, cuentasCanceladas });
+*/
+//const initialState = { socios, sociedades, terceros, cuentasVigentes, cuentasCanceladas };
+
+
 
 const initialState = {
   'socios': [
